c555fa7304901e6aaf2303c9e12a132884c6328b
Tatsuya Oiwa <tatsuyaoiw@gmail.com>
2014-10-08 08:32:11 +0900

    remove sublime2

--- sublime2/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Setup a machine for Sublime Text 2
-set -x
-
-# symlink settings in
-SUBLIME_DIR=~/Library/Application\ Support/Sublime\ Text\ 2/Packages
-rsync -av ./User/. "$SUBLIME_DIR/User"

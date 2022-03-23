f9465d01dc8f6c1a0381bc3ba45fb54bf5af5ad1
Tatsuya Oiwa <tatsuya.oiwa@shopify.com>
2022-03-23 12:40:53 -0400

    Use Oh My Zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,5 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-git pull
-function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
-}
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+#! /usr/bin/env sh
+
+if [ -z "$ZSH" ]; then
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
-unset doIt
-source ~/.bash_profile
